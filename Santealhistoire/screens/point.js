5c6a4be1af266dffb66a0a3e7a8139af235635c7
hervouetdylan <dylan.hervouet@ynov.com>
2023-05-15 14:06:46 +0200

    ajout des scores

---
+++ Santealhistoire/screens/point.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, Button } from "react-native/types";
+
+import AddPlayerScreen from "./AddPlayersScreen";
+
+export default function score(){
+    return(
+        <View>
+            <Text>Test</Text>
+        </View>
+    );
+}
